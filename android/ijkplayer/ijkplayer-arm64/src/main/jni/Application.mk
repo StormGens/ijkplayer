592339ef46b0aff965578f692baa7a2b7c4e9104
张利强 <liqiangzhang@xiaohongshu.com>
2021-02-28 23:41:50 +0800

    fix:修改Android.mk文件，使其可以被IDE识别为源码文件

--- android/ijkplayer/ijkplayer-arm64/src/main/jni/Application.mk
+++ android/ijkplayer/ijkplayer-arm64/src/main/jni/Application.mk
@@ -25,7 +25,7 @@ APP_PIE := false
 
 APP_STL := stlport_static
 
-APP_CFLAGS := -O3 -Wall -pipe \
+APP_CFLAGS := -O0 -Wall -pipe \
     -ffast-math \
     -fstrict-aliasing -Werror=strict-aliasing \
     -Wno-psabi -Wa,--noexecstack \
